6aaea7a421849d2e54fbd31c01f60ab9b6f880b7
Andrew Toussaint <ajntoussaint@gmail.com>
2024-11-07 14:39:23 -0600

    Removed navigating away if the user is not logged in, context is not working, keep getting loading undefined and reading user as null when a page loads

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -4,14 +4,25 @@ import { useUser } from './UserContext';
 
 //wraps routes that require auth
 const ProtectedRoute = ({ children }) => {
-  const { user } = useUser();
+  const { user, loading } = useUser();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user) {
-      navigate('/login');
+
+    if (!loading) {
+      if (!user) {
+        console.log("kicked out: ", user, loading);
+        navigate('/login');
+      } else {
+        console.log("let in: ", user, loading);
+      }
     }
-  }, [user, navigate]);
+
+  }, [user, loading, navigate]);
+
+   if (loading) {
+    return <div>Loading...</div>; // Or any loading indicator
+  }
 
   return user ? children : null; // Render children if user is signed in
 };
--- client/src/components/UserContext.js
+++ client/src/components/UserContext.js
@@ -1,12 +1,26 @@
 //A unified context for determining whether the user is logged in in every component
 
 //not suitable for authentication, to authenticate GET request to /user
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import axiosInstance from '../modules/axiosInstance';
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUser(){
+      setLoading(true);
+      const { data } = await axiosInstance.get("/user");
+      console.log("Context fetched user: " , data);
+      setUser(data);
+      setLoading(false);
+    }
+
+    fetchUser();
+  },[]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
